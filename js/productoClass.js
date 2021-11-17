f1a9737ac6a10cffeb588b55eaeee9285283a5d3
dav1dM3rcado <91168753+dav1dM3rcado@users.noreply.github.com>
2021-11-17 09:43:18 -0300

    agragamos los get y set

--- js/productoClass.js
+++ js/productoClass.js
@@ -7,4 +7,35 @@ class Producto {
     this.url = campoURL;
   }
   // agregar los get y set
+  get mostrarCodigo() {
+    return this.codigo;
+  }
+  get mostrarProducto() {
+    return this.Producto;
+  }
+  get mostrarDescripcion() {
+    return this.descripcion;
+  }
+  get mostrarCantidad() {
+    return this.cantidad;
+  }
+  get mostrarUrl() {
+    return this.url;
+  }
+  //Los SET
+  set modificarCodigo(nuevoCodigo) {
+    this.codigo = nuevoCodigo;
+  }
+  set modificarProducto(nuevoProducto) {
+    this.producto = nuevoProducto;
+  }
+  set modificarDescripcion(nuevaDescripcion) {
+    this.descripcion = nuevaDescripcion;
+  }
+  set modificarCantidad(nuevaCantidad) {
+    this.cantidad = nuevaCantidad;
+  }
+  set modificarURL(nuevaURL) {
+    this.url = nuevaURL;
+  }
 }
